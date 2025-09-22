c1486eace9b184991dfc2db0d1fff1b06816c8ca
Cristiaann19 <huamancruzcristianjesus@gmail.com>
2025-09-22 16:52:34 -0500

    HOLA

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.demo.systemair {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens Clases to javafx.fxml;
+    exports Clases;
+}
